a889bca73a0fe296c38ec6665db13946901e305a
Matt Moore <illogical@users.noreply.github.com>
2022-07-17 18:20:53 -0400

    Add lobby

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,8 +1,11 @@
+using Mirror;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject landingPagePanel;
+
     void Start()
     {
         
@@ -11,5 +14,17 @@ public class MainMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void HostLobby()
+    {
+        landingPagePanel.SetActive(false);
+
+        //if (useSteam)
+        //{
+        //    SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, maxPlayers);
+        //}
+
+        NetworkManager.singleton.StartHost();
     }
 }
